4f8540e714e3ade7ff1be3be4d25d464aec09b81
Perica Bozinoski <boz.develop@gmail.com>
2020-04-25 23:35:08 -0400

    Added new features

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ Change the game to follow these rules:
 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 */
 var scores, roundScore, activePlayer, gamePlaying,nameOne,nameTwo, gameLength;
+
+//----- Get User Values
 nameOne = document.querySelector('#player-0-name').value;
 nameTwo = document.querySelector('#player-1-name').value;
 gameLength = document.querySelector('#game-length').value;
+
 document.querySelector('#win-score').textContent = gameLength;
 
 document.querySelector('.btn-close').addEventListener('click',function(){
@@ -20,11 +23,47 @@ document.getElementById('name-0').textContent = nameOne;
 document.getElementById('name-1').textContent = nameTwo;
 document.querySelector('#win-score').textContent = gameLength;
 });
+let player1 = [0,0,0];
+let player2 = [0,0,0];
+let score0Save = 0;
+let score1Save = 0;
+let doubleArr = [];
+let saved = false;
+let canSave = false;
 
 init();
 
+
+function updateSavedScore(){
+    if(activePlayer === 0){
+        if(player1[0] === player1[1] && player1[0] === player1[2]){
+            score0Save = (player1[0]+player1[1]+player1[2])*3;
+        }else if(player1[0] === player1[1] || player1[0] === player1[2] && player1 !== 0){
+            score0Save = (player1[0]+player1[1]+player1[2])*2
+        }else if(player1[1] === player1[2] && player1[1] !== 0){
+            score0Save = (player1[0]+player1[1]+player1[2])*2
+        }else{
+            score0Save = (player1[0]+player1[1]+player1[2]);
+        }
+    }else if(activePlayer === 1){
+        if(player2[0] === player2[1] && player2[0] === player2[2]){
+            score1Save = (player2[0]+player2[1]+player2[2])*3
+        }else if(player2[0] === player2[1] || player2[0] === player2[2] && player2 !== 0){
+            score1Save = (player2[0]+player2[1]+player2[2])*2
+        }else if(player2[1] === player2[2] && player2[1] !== 0){
+            score1Save = (player2[0]+player2[1]+player2[2])*2
+        }else{
+            score1Save = (player2[0]+player2[1]+player2[2]);
+        }
+    }
+    document.querySelector('#score-0-save').textContent = score0Save;
+    document.querySelector('#score-1-save').textContent = score1Save;
+}
+
 document.querySelector('.btn-roll').addEventListener('click',function(){
     if(gamePlaying){
+        saved = false;
+        canSave = false;
 // 1. Generate random number
     var dice1 = Math.floor(Math.random() * 6) + 1;
     var dice2 = Math.floor(Math.random() * 6) + 1;
@@ -42,10 +81,12 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     nextPLayer();
     }
     else if(dice1 === dice2){
-        
+        canSave = true;
         roundScore+=(dice1*4);
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
-
+        dice1DOM.src = `dice-${dice1}-save.png`;
+        dice2DOM.src = `dice-${dice2}-save.png`;
+        doubleArr.push(dice1);
     }
     else if(dice1 !== 1 && dice2 !== 1){
         roundScore += dice1+dice2;
@@ -56,6 +97,33 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
         }
     }
 });
+
+document.querySelector('#dice-1').addEventListener("click",function(){
+    if(activePlayer === 0 && !saved && canSave){
+        saved = true;
+        for(let i = 0;i < player1.length;i++){
+            if(player1[i] === 0){
+                player1[i] = doubleArr[doubleArr.length-1];
+                document.querySelector(`#hold-0-${i}`).src = `dice-${doubleArr[doubleArr.length-1]}-save.png`;
+                updateSavedScore()
+                attack();
+                break;
+            }
+        }
+    }
+    else if(activePlayer === 1 && !saved && canSave){
+        saved = true;
+        for(let i = 0;i < player2.length;i++){
+            if(player2[i] === 0){
+                player2[i] = doubleArr[doubleArr.length-1];
+                document.querySelector(`#hold-1-${i}`).src = `dice-${doubleArr[doubleArr.length-1]}-save.png`;
+                updateSavedScore()
+                attack();
+                break;
+            }
+        }
+    }
+})
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
     if(gamePlaying){
@@ -88,6 +156,8 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
 document.querySelector('.btn-new').addEventListener('click',init)
 
     function init(){
+        player1 = [0,0,0];
+        player2 = [0,0,0];
         scores = [0,0];
         roundScore = 0;
         activePlayer = 0;
@@ -105,6 +175,16 @@ document.querySelector('.btn-new').addEventListener('click',init)
         document.querySelector('.player-1-panel').classList.remove('active');
         document.querySelector('.player-0-panel').classList.add('active');
         document.querySelector('.options').style.display = 'none';
+        document.querySelector('#hold-0-0').src = `dice-empty.png`;
+        document.querySelector('#hold-0-1').src = `dice-empty.png`;
+        document.querySelector('#hold-0-2').src = `dice-empty.png`;
+        document.querySelector('#hold-1-0').src = `dice-empty.png`;
+        document.querySelector('#hold-1-1').src = `dice-empty.png`;
+        document.querySelector('#hold-1-2').src = `dice-empty.png`;
+        document.querySelector('#score-0-save').textContent = 0;
+        document.querySelector('#score-1-save').textContent = 0;
+        document.querySelector('.attack-0').style.display = "none";
+        document.querySelector('.attack-1').style.display = "none";
         gamePlaying = true;
     };
     document.querySelector('.btn-options').addEventListener('click',function(){
@@ -123,5 +203,60 @@ document.querySelector('.btn-new').addEventListener('click',init)
         document.querySelector('#dice-2').style.display = 'block';
         };
         document.querySelector()
-        
         });
+
+        function attack(){
+            if(player1[0] !== 0 && player1[1] !== 0 && player1[2] !== 0){
+                document.querySelector('.attack-0').style.display = "block";
+            }else if(player2[0] !== 0 && player2[1] !== 0 && player2[2] !== 0){
+                document.querySelector('.attack-1').style.display = "block";
+            }
+        }
+
+        document.querySelector('.attack-0').addEventListener("click",function(){
+            if(activePlayer === 0 && score0Save < scores[1]){
+                scores[1]-=score0Save;
+                document.getElementById('score-1').textContent = scores[1];
+                document.querySelector('#hold-0-0').src = `dice-empty.png`;
+                document.querySelector('#hold-0-1').src = `dice-empty.png`;
+                document.querySelector('#hold-0-2').src = `dice-empty.png`;
+                player1 = [0,0,0];
+                score0Save = 0;
+                document.querySelector('.attack-0').style.display = "none";
+                document.querySelector('#score-0-save').textContent = 0;
+            }else if(activePlayer === 0 && score0Save >= scores[1]){
+                scores[1] = 0;
+                document.getElementById('score-1').textContent = scores[1];
+                document.querySelector('#hold-0-0').src = `dice-empty.png`;
+                document.querySelector('#hold-0-1').src = `dice-empty.png`;
+                document.querySelector('#hold-0-2').src = `dice-empty.png`;
+                player1 = [0,0,0];
+                score0Save = 0;
+                document.querySelector('.attack-0').style.display = "none";
+                document.querySelector('#score-0-save').textContent = 0;
+            }
+        })
+        document.querySelector('.attack-1').addEventListener("click",function(){
+            if(activePlayer === 1 && score1Save < scores[0]){
+                scores[0]-=score1Save;
+                document.getElementById('score-0').textContent = scores[0];
+                document.querySelector('#hold-1-0').src = `dice-empty.png`;
+                document.querySelector('#hold-1-1').src = `dice-empty.png`;
+                document.querySelector('#hold-1-2').src = `dice-empty.png`;
+                player2 = [0,0,0];
+                score1Save = 0;
+                document.querySelector('.attack-1').style.display = "none";
+                document.querySelector('#score-1-save').textContent = 0;
+
+            }else if(activePlayer === 1 && score1Save >= scores[0]){
+                scores[0] = 0;
+                document.getElementById('score-0').textContent = scores[0];
+                document.querySelector('#hold-1-0').src = `dice-empty.png`;
+                document.querySelector('#hold-1-1').src = `dice-empty.png`;
+                document.querySelector('#hold-1-2').src = `dice-empty.png`;
+                player2 = [0,0,0];
+                score1Save = 0;
+                document.querySelector('.attack-1').style.display = "none";
+                document.querySelector('#score-1-save').textContent = 0;
+            }
+        })
